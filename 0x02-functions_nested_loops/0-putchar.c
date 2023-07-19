407976082050b8ab99cda5ce89de282ba8d6c59b
onyibaybay <onyinyechukwuedith090@gmail.com>
2023-07-19 07:09:24 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _putchar - This is a function
+ * main - Entry point
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
