f2b611f4e88848c4586389ec13a8030f86b85a6e
onyibaybay <onyinyechukwuedith090@gmail.com>
2023-07-21 16:17:26 -0700

    7print

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+include "main.h"
+
+/**
+ * print_square - unction that prints a square
+ * @size: the size of the square
+ * Return: empty
+ */
+void print_square(int size)
+{
+	int j;
+
+	int i;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+		for (j = 0; j < size; j++)
+		{
+			for (i = 0; i < size; i++)
+			{
+				_putchar(35);
+			}
+		}
+}
