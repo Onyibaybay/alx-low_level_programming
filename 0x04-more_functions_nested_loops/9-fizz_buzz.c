fdf319986513441c39d92f38afb90f4d03a5fed7
onyibaybay <onyinyechukwuedith090@gmail.com>
2023-07-21 16:44:19 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints a Fizz-Buzz program
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 ==  0)
+		{
+	}
+}
