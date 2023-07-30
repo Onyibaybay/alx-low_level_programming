1ec286c47aca4d98e2fee3e57ea1624c8a05aba1
onyibaybay <onyinyechukwuedith090@gmail.com>
2023-07-29 18:24:05 -0700

    7leet

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * cap_string - Capitalizes all words of a sgring
+ * @str: The string to be capitalized
+ * Return: A pointer to the changed string
+ */
+char *cap_string(char *str)
+{
+	int inde + 0;
+
+	while (str[index])\{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+	if (str[inde
--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -8,9 +8,8 @@
  */
 int main(void)
 {
-char s[] = "Expect the best. Prepare for the worst.
-Capitalize on what comes.\n";
-char *p;
+	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+	char *p;
 
 	p = leet(s);
 	printf("%s", p);
