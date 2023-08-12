3d5edbd5db62e9ee6e9a3184da9c9f426009ed8a
onyibaybay <onyinyechukwuedith090@gmail.com>
2023-08-11 19:21:33 -0700

    1-string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * string_nconcat - The function that concatenate two strings
- * @s1 - first string
- * @s2 - second string
- *
- * Return - if fail should return Null
+ * @s1: first string
+ * @s2: second string
+ * @n: n element of s2
+ * Return: if fail should return Null
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -16,27 +17,30 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int lens = 0;
 
 	if (s1 == NULL)
-	       s1 = "";
+		s1 = "";
 
-if (s1 == NULL)
-s2 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-while (s1[len1] != '\0')
-	len2++;
+	while (s1[len1] != '\0')
+		len1++;
 
-if (n >= len2)
-	n = len2;
+	while (s2[lens] != '\0')
+		lens++;
+	if (n >= lens)
+	n = lens;
 
-s = malloc(sizeof (char) * (len1 + n + 1));
+	s = malloc(sizeof(char) * (len1 + n + 1));
 
-if (s == NULL)
+	if (s == NULL)
 	return (NULL);
 
-for (a = 0; a < len1; a++)
+	for (a = 0; a < len1; a++)
 	s[a] = s1[a];
-for (a = 0; a < n; a++)
-	s[len1 + a] = s2[a];
-s[len1 + n] = '\0';
 
-	return ();
+	for (a = 0; a < n; a++)
+	s[len1 + a] = s2[a];
+	s[len1 + n] = '\0';
+
+	return (s);
 }
