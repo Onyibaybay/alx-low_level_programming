cd1eb0c0ca3d6fe4506541efa349dac49b23ac62
onyibaybay <onyinyechukwuedith090@gmail.com>
2023-09-08 18:07:14 -0700

    1 create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,46 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include "main.h"
 
 /**
  * create_file - functions that create a file with the specified permissions
  * @filename: name of to create
  * @text_content: content writed in the file.
  *
- * Returns: 1 on success, -1 on failure.file cannot be created, written,
+ * Return: 1 on success, -1 on failure.file cannot be created, written,
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor;
-	size_t nletters;
-	ssize_t bytes_written;
-	int rwr;
+	int temp, store, i, length = 0;
 
 	if (filename == NULL)
-		return (-1);
-
-	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-
-	if (file_descriptor == -1)
-		return (-1);
-
-	if (text_content != NULL)
-		size_t nletters = strlen(text_content);
-	bytes_written = write(file_descriptor, text_content, nletters);
-
-	if (text_content != NULL)
 	{
-		rwr = write(file_descriptor, text_content, nletters);
-
-		if (rwr == -1)
-			return (-1);
-		{
-			close(file_descriptor);
-			return (-1);
-		}
+		return (-1);
 	}
-	close(file_descriptor);
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+	store = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (store == -1)
+	{
+		return (-1);
+	}
+	for (i = 0; text_content[i] != '\0'; i++)
+	{
+		length++;
+	}
+	temp = write(store, text_content, length);
+
+	if (temp == -1)
+	{
+		return (-1);
+	}
+	close(store);
 	return (1);
 }
